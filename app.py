fcaa1da601099703bd80afd68c6b8e874bb98f37
Ram Kansal <ramkansal2502@gmail.com>
2024-11-16 21:48:08 +0530

    Changed Small Things...

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=443, ssl_context=('fullchain.pem', 'privkey.pem'))
+    app.run(host='0.0.0.0', port=5000)
